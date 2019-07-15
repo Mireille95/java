275713005525bdf3d656daa626abfb174db4980d
mireille <mimimireille95@gmail.com>
2019-07-15 11:20:47 -0400

    java with function and string

---
+++ index.js
@@ -0,0 +1,94 @@
+// var someName="mireille";
+// console.log("mireille");
+// var mFav= 2;
+// var div= 0;
+// div= mFav / 2;
+// sub= mFav - 2;
+// mult= mFav * 2;
+
+// console.log(div);
+// console.log(sub);
+// console.log(mult);
+// var Hello="hello world";
+
+
+//  98.4.toExponential();
+//  737.255342.toFixed();
+//  sevenHundredEightyNine.toString();
+
+// console.log(Hello);
+// var word="I love JavaScript";
+// word.toUpperCase();
+
+// "pine".concat("apple");
+
+///string to search
+
+// var stringToSearch = "Please show me where 'show' is located!";
+
+// var a = stringToSearch.search("show");
+// console.log(a);
+ 
+/// concatenation and character number
+
+// var fName="Umutoniwase";
+// var lName="Mireille";
+// var fullName= fName + lName;
+// "Umutoniwase" .concat("Mireille");
+// "Mireille".charAt(1);
+// console.log(fullName);
+
+
+// upper and lower case
+// var fName="Umutoniwase";
+// var lName="Mireille";
+
+// var a= fName.toUpperCase();
+// var b= lName.toLowerCase();
+// console.log(a);
+// console.log(b);
+
+
+///string NaN
+
+// var name = "Andrea";
+// var nonsense = parseInt(name);
+// nonsense;
+// console.log(nonsense);
+
+///string prompt
+// var mySchool = prompt("Where do you go to school?");
+// mySchool;
+// console.log(mySchool);
+
+
+/// string prompt with upper case
+// var mySchool= prompt("Are you ready to code?").toUpperCase();
+// console.log(mySchool);
+
+//string comfirm
+//  var mySchool = confirm("Would you like to continue?");
+// console.log(mySchool);
+
+///prompt ages
+
+// var age = prompt("Enter your age: ");
+//  var sum = age + age;
+//  console.log(sum);
+
+
+/// parse string
+// var number = parseInt("5");
+//  number;
+//  console.log(number);
+
+/// function  prompt , parse string
+// var inputtedAge = prompt("Enter your age: ");
+// var age = parseInt(inputtedAge);
+// age + age;
+// console.log(age);
+
+/// parsefloat function
+// var pi = "3.14";
+//  parseFloat(pi);
+//  console.log(pi);
